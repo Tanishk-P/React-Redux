afc8767a013da4d24cbb24a13803d0d5f22c409d
Tanishk P <tanisk@maxscriptstechnologies.com>
2023-03-02 17:49:57 +0530

    Later changes.

--- src/Components/AddForm.js
+++ src/Components/AddForm.js
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addPosts } from "../Actions/postActions";
+import logo from "../images/note.png";
 
 function AddForm() {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const dispatch = useDispatch();
-    // const newPosts = useSelector((state) => state.posts.item )
-
-    // useEffect(() => {
-    //     dispatch()
-    // }, [])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -25,18 +21,21 @@ function AddForm() {
         <h1>Add Posts:</h1>
         <div>
           <label>
-            Post Title:
             <input name="title" value={title} onChange={e => setTitle(e.target.value)} placeholder="Enter a post title" />
           </label>
         </div>
         <br />
         <div>    
           <label>
-            Post Body:
             <textarea name="body" value={body} onChange={e => setBody(e.target.value)} placeholder="Enter body" />
           </label>
         </div>
         <br />
+        <div>
+          <label htmlFor='file'>
+            <img src={logo} style={{height:'22px',width:'22px'}} alt='Add your Post Image'/>
+          </label>
+        </div>
         <div>
           <button type="submit" >Submit</button>
         </div>
--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getPosts } from "../Actions/postActions";
-import logo from "../images/note.png"
 
 function PostList() {
   const dispatch = useDispatch();
@@ -25,8 +24,7 @@ function PostList() {
   return (
     <div>
       <h1>Posts</h1>
-      <input id='file' type={'file'} hidden />
-      <label htmlFor='file'><img src={logo} style={{height:'22px',width:'22px'}} alt='Add your Post Image'/></label> 
+      <input id='file' type={'file'} hidden /> 
     </div>
   );
 }
